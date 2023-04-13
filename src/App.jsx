0b8a3e36c8386dbc7eb5db82c1c8418ec5d01655
Antonio Alan <tonio.alan07@gmail.com>
2023-04-13 16:48:01 -0300

    Ajustes iniciais na tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import Login from './pages/Login'
+import Main from './pages/Main'
 
 function App() {
   
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function Login() {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  );
+}
+
+export default Login;
