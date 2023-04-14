90696686745100a1edaafd9b79c084b5ba88881f
Antonio Alan <tonio.alan07@gmail.com>
2023-04-14 04:08:16 -0300

    titles do Page Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,8 +2,13 @@ import styles from './styles.module.css'
 
 function Login() {
   return (
-    <div className={styles.container}>
-        
+    <div className={styles.container_title}>
+      <h1>
+        Welcome to CodeLeap network!
+      </h1>
+      <h2>
+        Please enter your username
+      </h2>
     </div>
   );
 }
