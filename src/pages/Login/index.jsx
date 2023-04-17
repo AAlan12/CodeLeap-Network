d98ad11c6a42b28505a3b450ec5fc81dda3de5d0
Antonio Alan <tonio.alan07@gmail.com>
2023-04-16 22:38:04 -0300

    Ajustes no css raiz e na Page Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,12 +3,15 @@ import Input from '../../components/Input';
 
 function Login() {
   return (
-    <div className={styles.container_title}>
-      <h1>
-        Welcome to CodeLeap network!
-      </h1>
-      <Input title="Please enter your username" type="email" placeholder="John doe" />
+    <div className={styles.container}>
+      <div className={styles.container_login}>
+        <h1>
+          Welcome to CodeLeap network!
+        </h1>
+        <Input title="Please enter your username" type="email" placeholder="John doe" />
+      </div>
     </div>
+
   );
 }
 
