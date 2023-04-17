082b74c4b72ca0d1378778d1c53d23f8b27b6e63
Antonio Alan <tonio.alan07@gmail.com>
2023-04-16 23:41:22 -0300

    Ajustes no css

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,10 +2,12 @@ import styles from './styles.module.css'
 
 function Main() {
   return (
-    <div className={styles.container_title}>
-      <h1>
-        CodeLeap Network
-      </h1>
+    <div className={styles.container}>
+      <div className={styles.container_title}>
+        <h1>
+          CodeLeap Network
+        </h1>
+      </div>
     </div>
   );
 }
