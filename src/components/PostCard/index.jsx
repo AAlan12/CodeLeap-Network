e0baed42e4efff1e4c2f62fd2f2ea79e12369726
Antonio Alan <tonio.alan07@gmail.com>
2023-04-20 09:20:01 -0300

    Criação do component PostCard

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+function PostCard({ title, username, content }) {
+  return <div />;
+}
+
+export default PostCard;
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,13 +2,13 @@ import styles from './styles.module.css';
 import Titles from '../../components/Titles';
 import Input from '../../components/Input';
 import Buttons from '../../components/Buttons';
+import PostCard from '../../components/PostCard';
 
 function Main() {
   return (
     <div className={styles.container}>
       <div className={styles.container_title}>
         <Titles title="CodeLeap Network" />
-       
       </div>
       <div className={styles.container_block}>
         <div className={styles.container_form}>
@@ -18,6 +18,14 @@ function Main() {
             <Input title="Content" type="text" placeholder="Content here" />
           </div>
           <Buttons text="Create"/>
+        </div>
+        <div className={styles.container_post}>
+          <PostCard 
+                  title="My First Post at CodeLeap Network!" 
+                  username="Sir Alonne" 
+                  content="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+
+                  Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat." />
         </div>
       </div>
     </div>
