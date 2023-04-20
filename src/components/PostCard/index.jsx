e8619261061fdf9c2e1334c97962ef858228b1cf
Antonio Alan <tonio.alan07@gmail.com>
2023-04-20 11:39:10 -0300

    container_content no component PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -3,16 +3,24 @@ import Titles from '../Titles';
 import IcoDel from '../../assets/ic-delete.svg'
 import IcoEdit from '../../assets/ic-edit.svg'
 
-function PostCard({ title, username, content }) {
+function PostCard({ title, username, time, content }) {
   return (
-    <div className={styles.container_header}>
-      <Titles title={title} />
-      <div className={styles.container_icons}>
-        <img src={IcoDel} alt="" />
-        <img src={IcoEdit} alt="" />
+    <>
+      <div className={styles.container_header}>
+        <Titles title={title} />
+        <div className={styles.container_icons}>
+          <img src={IcoDel} alt="" />
+          <img src={IcoEdit} alt="" />
+        </div>
       </div>
-
-    </div>
+      <div className={styles.container_content}>
+        <div className={styles.container_row}>
+          <h4>@{username}</h4>
+          <h3>{time} minutes ago</h3>
+        </div>
+        <p>{content}</p>
+      </div>
+    </>
   );
 }
 
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -15,6 +15,7 @@ function Main() {
           <PostCard 
                   title="My First Post at CodeLeap Network!" 
                   username="Sir Alonne" 
+                  time={25}
                   content="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
 
                   Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat." />
