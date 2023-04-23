6ca1e4398fecb4b815f85b08e11aadf8ecad4195
Antonio Alan <tonio.alan07@gmail.com>
2023-04-23 09:33:38 -0300

    Link para o main page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import styles from './styles.module.css';
 import Input from '../../components/Input';
 import Titles from '../../components/Titles';
 import Buttons from '../../components/Buttons';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
@@ -9,7 +10,9 @@ function Login() {
       <div className={styles.container_login}>
         <Titles title="Welcome to CodeLeap network!"/>
         <Input title="Please enter your username" type="email" placeholder="John doe" />
-        <Buttons text="ENTER"/>
+        <Link to="/main">
+          <Buttons text="ENTER"/>
+        </Link>
       </div>
     </div>
   );
