0428bd5de08282077798e377748adaf4a5e0accf
Antonio Alan <tonio.alan07@gmail.com>
2023-04-24 09:19:31 -0300

    Criação do botão de sair

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,21 +4,26 @@ import Form from '../../components/Form';
 import PostCard from '../../components/PostCard';
 import DeleteScreen from '../../components/DeleteScreen';
 import EditScreen from '../../components/EditScreen';
+import { BsBoxArrowLeft } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 function Main() {
   return (
     <div className={styles.container}>
       <div className={styles.container_title}>
         <Titles title="CodeLeap Network" />
+        <Link to="/">
+          <BsBoxArrowLeft />
+        </Link>
       </div>
       <div className={styles.container_block}>
         <Form title="Whats on your mind" btn="Create" />
         <div className={styles.container_post}>
-          <PostCard 
-                  title="My First Post at CodeLeap Network!" 
-                  username="Sir Alonne" 
-                  time={25}
-                  content="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+          <PostCard
+            title="My First Post at CodeLeap Network!"
+            username="Sir Alonne"
+            time={25}
+            content="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
 
                   Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat." />
         </div>
